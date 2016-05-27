e4b055ca01fab5195053dd4c1f18868d162be989
Marek Cerny <c.markus@seznam.cz>
2016-05-27 18:15:33 +0200

    setting - save button

--- app/pages/settings/settings.js
+++ app/pages/settings/settings.js
@@ -1,4 +1,5 @@
 import {Page, NavController} from 'ionic-angular';
+import {CampaignsListPage} from '../campaigns-list/campaigns-list';
 
 @Page({
 	templateUrl: 'build/pages/settings/settings.html',
@@ -11,9 +12,14 @@ export class SettingsPage {
 	constructor(nav) {
 		this.nav = nav;
 		this.language = localStorage.getItem('language') || 'cs';
+		this.rootPage = CampaignsListPage;
 	}
 
 	setLanguage(language) {
 		localStorage.setItem('language', language);
+	}
+
+	redirect() {
+		this.nav.setRoot(this.rootPage);
 	}
 }
